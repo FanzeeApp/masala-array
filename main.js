3c50b752c9b21f1e2b76319bbcb90152f2c08af2
FanzeeApp <fanzeepubgm@gmail.com>
2024-10-09 06:26:28 +0500

    .

--- main.js
+++ main.js
@@ -36,31 +36,28 @@
 
 // // arr 7
 
-// let kirit = [2, 3, 4, 6, 8, 76, 45, 6];
-// console.log(kirit.indexOf(76));
+// let str = "Uzbekistan";
+// natija = str.length;
+// console.log(natija);
 
 // // arr 8
 
-// let kirit = [1, 2, 3, 4, 5];
+// let kirit = [2, 3, 4, 5];
 
-// let javob = kirit.slice(1, 3);
-// console.log(javob);
+// kirit.unshift(1);
+// console.log(kirit);
 
 // // arr 9
 
-// let yigndi = [5, 10, 14, 56];
-// let javob = 0;
-// for (let i = 0; i < yigndi.length; i++) {
-//   javob += yigndi[i];
-// }
+// let java = "JAVASCRIPT";
+// let javob = java.toLowerCase();
 // console.log(javob);
 
 // // arr 10
 
-// let str = "123-456-789";
-// let javob = str.split("-");
-// let matn = javob.join(" ");
-// console.log(matn);
+// let arr = ["cherry", "banana", "apple"];
+// let rec = arr.reverse();
+// console.log(rec);
 
 // // arr 11
 
@@ -102,7 +99,123 @@
 
 //////////////////////////////////////////////////
 
-// // hard 1
+// // medium 1
+
+// let arr = [5, 2, 8, 9, 1, 7];
+// let cher = arr.sort();
+// console.log(cher);
+
+// // medium 2
+
+// let arr = "apple,banana,cherry";
+// let sin = arr.split(",");
+// console.log(sin);
+
+// // medium 3
+
+// let ar = [1, 2, 3, 4, 5, 6, 8];
+// let sonlar = ar.includes(5);
+
+// console.log(sonlar);
+
+// // medium 4
+
+// let ar = [1, 2, 3, 4, 5, 6, 7, 8];
+// let qirqish = ar.slice(1, 3);
+
+// console.log(qirqish);
+
+// // medium 5
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let javob = [];
+// let ish = arr.forEach((num) => {
+//   if (num % 2 === 0) {
+//     javob.push(num);
+//   } else {
+//   }
+// });
+// console.log(javob);
+
+// // medium 6
+
+// let strr = "hello world";
+// let javob = strr.includes("world");
+// console.log(javob);
+
+// // medium 7
+
+// let arr = [10, 20, 30, 40];
+// function nom(num) {
+//   let ishlash = arr.indexOf(num);
+//   return ishlash;
+// }
+// console.log(nom(40));
+
+// // medium 8
+
+// let ar = [1, 2, 3, 4, 5, 6, 7, 8];
+// let qirqish = ar.slice(1, 3);
+
+// console.log(qirqish);
+
+// medium 9
+
+// let yigndi = [5, 10, 14, 56];
+// let javob = 0;
+// for (let i = 0; i < yigndi.length; i++) {
+//   javob += yigndi[i];
+// }
+// console.log(javob);
+
+// // medium 10
+
+// let str = "123-456-789";
+// let javob = str.split("-");
+// let matn = javob.join(" ");
+// console.log(matn);
+
+// // medium 11
+
+// let arr = [1, 5, 3, 9, 2, 14, 6, 12];
+// let hisob = Math.max(...arr);
+// console.log(hisob);
+
+// // medium 12
+
+// let arr = ["Banana", "Apple", "orange", "Limon"];
+// let nev = arr.sort();
+// console.log(nev);
+
+// // medium 13
+
+// arr = [2, 4, 6, 8];
+// javob = true;
+// let nev = arr.forEach((num) => {
+//   if (num % 2 === 1) {
+//     javob = false;
+//   }
+// });
+
+// console.log(javob);
+
+// // medium 14
+
+// let str = "javascript";
+// let teskari = str.split("").reverse().join("");
+// console.log(teskari);
+
+// // medium 15
+
+// let arr = [1, 2, 3, 4, 5];
+// let sup = arr.map((n) => {
+//   return n + 2;
+// });
+// console.log(sup);
+
+//////////////////////////////////////////////////////////
+
+// // HARD++ 1
 
 // let str = "madam";
 // let lower = str.toLocaleLowerCase();
@@ -113,7 +226,7 @@
 //   console.log(false);
 // }
 
-// // hard 2
+// // HARD++ 2
 
 // let n = 3;
 
@@ -124,19 +237,40 @@
 
 // console.log(cl);
 
-// // hard 3
-
+// // HARD++ 3
 // let str = "apple banana apple orange banana";
 // let javob = str.split(" ");
 
 // let tenglash = javob.filter((meva, index) => javob.indexOf(meva) === index);
 // console.log(tenglash);
 
-// hard 4
+// // HARD++ 4
 
-let arr = [1, [2, 3], [4, [5, 6]]];
-let list = [];
-let javob = arr.forEach((num) => {
-  list.push(num);
-  console.log(list);
-});
+// function aylanma(arr) {
+//   let natija = [];
+//   arr.forEach(function (kirit) {
+//     if (Array.isArray(kirit)) {
+//       natija = natija.concat(aylanma(kirit));
+//     } else {
+//       natija.push(kirit);
+//     }
+//   });
+//   return natija;
+// }
+
+// const kiritish = [1, [2, 3], [4, [5, 6]]];
+// const chiqish = aylanma(kiritish);
+// console.log(chiqish);
+
+// // HARD 5++
+
+// let arr = "hello world from java Script";
+// let javob = arr.split(" ");
+// oxirgi = [];
+// let boshHarf = javob.forEach((n) => {
+//   let bosh = n.charAt(0).toLocaleUpperCase();
+//   let song = n.slice(1);
+//   let oxiri = bosh + "" + song;
+//   oxirgi.push(oxiri);
+// });
+// console.log(oxirgi.join(" "));
